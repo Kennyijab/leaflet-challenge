c38025d9cab0278f42555ba0e1755bbcfb1431c0
Olamide Aladeyelu <Olamide.Aladeyelu@ng.ey.com>
2024-07-16 05:27:53 +0100

    Add earthquake marker customization and legend

--- Leaflet-Part-1/app.js
+++ Leaflet-Part-1/app.js
@@ -6,26 +6,26 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Function to determine marker size based on magnitude
+function markerSize(magnitude) {
+    return magnitude * 4;
+}
+
+// Function to determine marker color based on depth
+function markerColor(depth) {
+    return depth > 90 ? "#ff5f65" :
+           depth > 70 ? "#fca35d" :
+           depth > 50 ? "#fdb72a" :
+           depth > 30 ? "#f7db11" :
+           depth > 10 ? "#dcf400" :
+                        "#a3f600";
+}
+
 // URL to fetch earthquake data
 var earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Fetch the data
 d3.json(earthquakeUrl).then(function(data) {
-    // Function to determine marker size based on magnitude
-    function markerSize(magnitude) {
-        return magnitude * 4;
-    }
-
-    // Function to determine marker color based on depth
-    function markerColor(depth) {
-        return depth > 90 ? "#ff5f65" :
-               depth > 70 ? "#fca35d" :
-               depth > 50 ? "#fdb72a" :
-               depth > 30 ? "#f7db11" :
-               depth > 10 ? "#dcf400" :
-                            "#a3f600";
-    }
-
     // Add a GeoJSON layer to the map
     L.geoJSON(data, {
         pointToLayer: function(feature, latlng) {
@@ -45,3 +45,24 @@ d3.json(earthquakeUrl).then(function(data) {
         }
     }).addTo(map);
 });
+
+// Add a legend to the map
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 30, 50, 70, 90],
+        labels = [];
+
+    div.innerHTML += "<h4>Depth (km)</h4>";
+
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + markerColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
